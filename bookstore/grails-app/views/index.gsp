799429a7f07028fd492ac2445b4e3ddaad51c053
Jeff Scott Brown <jeff@jeffandbetsy.net>
2015-06-26 22:43:53 -0500

    One approach to dynamic class instantiation
    The application is orchestrating things in a way that I don’t think is
    optimum but to simplify things and answer what I think the original
    question is about, the change in this commit may be reasonable.
    
    What I think is at least part of the original question is how can a
    plugin create an instance of a class that it can’t know about at
    compile time.  For example, how can a plugin create an instance of a
    class that is provided by the containing application.  In order to do
    that, somehow the plugin needs to be able to discover at runtime which
    class to create an instance of.  There are a number of ways this can be
    done.  This commit involves introducing support for the application to
    provide the name of a class in the configuration and the plugin can
    retrieve that value and use it.
    
    This commit leaves out reasonable error handling as that would only
    complicate the discussion.   This code assumes that the appropriate
    property is defined in application.yml.

--- bookstore/grails-app/views/index.gsp
+++ bookstore/grails-app/views/index.gsp
@@ -15,5 +15,10 @@
             params="[email: 'lee@example.com', password: '123456']">Create Bookstore User</g:link>
           (returns JSON)
       </p>
+  <p>
+     <g:link controller="bookstoreUser" action="register2"
+        params="[email: 'alvin@example.com', password: '123456']">Another Approach To Create Bookstore User</g:link>
+      (returns JSON)
+  </p>
     </body>
 </html>
--- security/grails-app/services/org/centerkey/grails/SecurityService.groovy
+++ security/grails-app/services/org/centerkey/grails/SecurityService.groovy
@@ -5,12 +5,28 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 class SecurityService {
 
    def encoder = new BCryptPasswordEncoder()
+   def userDomainClass
+   def grailsApplication
 
    def createUser(String email, String password) {
-      def user = new User(email: email, hash: encoder.encode(password))
+      def user = getUserDomainClass().newInstance(email: email, hash: encoder.encode(password))
       log.println "Register new user: \n   ${user as grails.converters.JSON}"
-      user.save(flush: true)  //MissingMethodException (because User is not the actual domain class BookstoreUser)
       return user
-      }
+   }
 
+   def createAndSaveUser(Map userInfo) {
+      def user = getUserDomainClass().newInstance(userInfo)
+      user.hash = encoder.encode(userInfo.password)
+      log.println "Register new user: \n   ${user as grails.converters.JSON}"
+
+      // return the saved user
+      user.save()
+  }
+
+   protected getUserDomainClass() {
+      if(!userDomainClass) {
+         userDomainClass = grailsApplication.getDomainClass(grailsApplication.config.myapp.securityDomainClass)?.clazz
+      }
+      userDomainClass
+   }
 }
